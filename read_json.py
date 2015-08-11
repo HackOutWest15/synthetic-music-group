9d42df454ded9ee47ecdf4c798ad9d554307d2fa
Anton Bergman <beanton@student.chalmers.se>
2015-08-11 12:12:06 +0200

    Added function to write real tone letters

--- read_json.py
+++ read_json.py
@@ -12,5 +12,21 @@ def get_duration_pitches():
 
 	return(segment_list)
 
-def get_highest_pitch(pitch_list):
-	return max(pitch_list)
+def get_duration_highest_pitch():
+	segment_list = []
+	with open('paint_it_black.json') as data_file:
+		data = json.load(data_file)
+		segments = data["segments"]
+		for segment in segments:
+			segment_list.append((segment["start"],segment["duration"], segment["pitches"].index(1.0)))
+
+	#pprint(segment_list)
+	return segment_list
+
+def get_duration_real_tones():
+	tonelist = ["C", "C#","D", "D#","E","F","F#","G","G#","A","A#","B"]
+	newlist = []
+	for tuple in get_duration_highest_pitch():
+		newlist.append((tuple[0], tuple[1], tonelist[tuple[2]]))
+	#pprint(newlist)
+	return newlist
