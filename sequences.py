03d57c953f40a62a98da7dfdd04098874c013ed6
Anton Bergman <beanton@student.chalmers.se>
2015-08-11 17:43:17 +0200

    Added support for multiple midi channels

--- sequences.py
+++ sequences.py
@@ -23,19 +23,30 @@ pib = midi.read_midifile("Paint_It_Black.mid")
 
 clk = Clock()
 
-track = pib[1]
-ticks = 0
+#track = pib[1]
+tracks = [pib[0], pib[1], pib[2], pib[3], pib[4]]
+#ticks = 0
 
-def next_noteonevent(current):
-    if isinstance(track[current], midi.NoteOnEvent):
+def next_noteonevent(current, pib_list):
+    if isinstance(pib_list[current], midi.NoteOnEvent):
         return current
     else:
-        return next_noteonevent(current+1)
+        return next_noteonevent(current+1, pib_list)
 
-idx = next_noteonevent(0)
-event = track[idx]
-ticks = event.tick
-data = event.data
+idxs = [next_noteonevent(0, tracks[0]), next_noteonevent(0, tracks[1]), next_noteonevent(0, tracks[2]), next_noteonevent(0, tracks[3]), next_noteonevent(0, tracks[4])]
+
+#event = track[idx]
+events = [tracks[0][idxs[0]], tracks[1][idxs[1]], tracks[2][idxs[2]], tracks[3][idxs[3]], tracks[4][idxs[4]]]
+
+#ticks = event.tick
+ticks = []
+for event in events:
+	ticks.append(event.tick) 
+
+#data = event.data
+#data = []
+#for event in events:
+#	data.append(event.data)
 
 dict = {'60':C, '61':Cs, '62':D, '63':Ds, '64': E, '65': F, '66': Fs, '67': G, '68': Gs, '69': A, '70':As,'71':B};
 
@@ -50,13 +61,12 @@ def play(data):
 while True:
     ms_passed = (clk.tick()*1.3)
 
-    ticks = ticks - ms_passed
+    for index, tick in enumerate(ticks):    	
+    	ticks[index] = tick - ms_passed
 
-    #print '.',
-
-    if ticks <= 0:
-        play(event.data)
-        idx = next_noteonevent(idx+1)
-        event = track[idx]
-        ticks = event.tick
-        data = event.data
+    	#print '.'
+    	if tick <= 0:
+	        play(events[index].data)
+	        idx = next_noteonevent(idxs[index]+1, tracks[index])
+	        events[index] = tracks[index][idx]
+	        ticks[index] = events[index].tick
