26fa3b2d7f39c55f83d61b6335525b15a7969a83
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2015-08-11 11:06:01 +0200

    add json decoding

--- get_song.py
+++ get_song.py
@@ -1,9 +1,15 @@
 from  pyechonest import *
 config.ECHO_NEST_API_KEY='CPS7RW8QAUIULBBVJ'
 
-paint_it_black = track.track_from_id('spotify:track:78Az0Z1vNJgqv9QSB0ULLV')
-dance_of_eternity = track.track_from_id('spotify:track:7FTf3bJuCq5UYHjUwggKNB')
+paint_it_black_id = 'spotify:track:78Az0Z1vNJgqv9QSB0ULLV'
+dance_of_eternity_id = 'spotify:track:7FTf3bJuCq5UYHjUwggKNB'
+
+def get_track(id_):
+    return track.track_from_id(id_)
 
 def get_analysis(track):
+    import urllib2
+    import json
     url = track.analysis_url
-    pass
+    response = urllib2.urlopen(url)
+    return json.loads(response.read())
