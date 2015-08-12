91b82ecfedc032cff3a0247c6a2f32745a9ba2d6
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2015-08-12 10:34:17 +0200

    make code beautiful

--- sequences.py
+++ sequences.py
@@ -6,7 +6,6 @@ from midi import events
 
 pygame.init()
 #pygame.mixer.init()
-
 pygame.mixer.init(frequency=22050, size=8, channels=2, buffer=2048)
 
 F4= pygame.mixer.Sound("Sounds/Glaspling/F5.wav")
@@ -47,65 +46,39 @@ A1 = pygame.mixer.Sound("Sounds/A1.wav")
 A1s = pygame.mixer.Sound("Sounds/A1#.wav")
 B1 = pygame.mixer.Sound("Sounds/B1.wav")
 
+tones = {'41': F1, '42': F1s, '43': G1, '44': G1s, '45': A1, '46':A1s,'47':B1,'48':C2, '49':C2s, '50':D2, '51':D2s, '52': E2, '53': F2, '54': F2s, '55': G2, '56': G2s, '57': A2, '58':A2s,'59':B2,'60':C3, '61':C3s, '62':D3, '63':D3s, '64': E3, '65': F3, '66': F3s, '67': G3, '68': G3s, '69': A3, '70':A3s,'71':B3,'72':C4,'73':C4s,'74':D4,'75':D4s,'76':E4,'77':F4};
+
+def play(event):
+    if isinstance(event, midi.NoteOnEvent) and event.data[1] != 0:
+        print "START " + str(event.data[0])
+        tones[str(event.data[0])].play()
 
 
-def next_noteonevent(current, pib_list):
-    if isinstance(pib_list[current], midi.NoteOnEvent):
-        return current
-    else:
-        return next_noteonevent(current+1, pib_list)
+def main():
 
+    song = midi.read_midifile("Paint_It_Black.mid")
+    clk = Clock()
 
-pib = midi.read_midifile("Paint_It_Black.mid")
-clk = Clock()
+    our_tracks = [0,1]
+    all_we_need = []
 
-tracks = []
-idxs = []
-events = []
+    for track_id in our_tracks:
+        track = song[track_id]
+        event = track[0]
+        tick = event.tick
+        all_we_need.append({'track': track, 'event_idx': 0, 'event': event, 'ticks_left': tick})
 
-for arg in sys.argv:
-	if(arg != "sequences.py"):
-		tracks.append(pib[int(arg)])
+    while True:
+        ticks_passed = (clk.tick()*1.3)
 
-for index, track in enumerate(tracks):
-	idxs.append(next_noteonevent(0, track))
-	events.append(tracks[index][idxs[index]])
+        for index, track in enumerate(all_we_need):
+            track['ticks_left'] = track['ticks_left'] - ticks_passed
 
-print "Events: "
-print events
-print "Idxs: "
-print idxs
+            if track['ticks_left'] <= 0:
+                play(track['event'])
+                track['event_idx'] = track['event_idx'] + 1
+                track['event'] = track['track'][track['event_idx']]
+                track['ticks_left'] = track['event'].tick
 
-#idxs = [next_noteonevent(0, tracks[0]), next_noteonevent(0, tracks[1]), next_noteonevent(0, tracks[2]), next_noteonevent(0, tracks[3]), next_noteonevent(0, tracks[4])]
-
-#events = [tracks[0][idxs[0]], tracks[1][idxs[1]], tracks[2][idxs[2]], tracks[3][idxs[3]], tracks[4][idxs[4]]]
-
-ticks = []
-for event in events:
-	ticks.append(event.tick)
-
-dict = {'41': F1, '42': F1s, '43': G1, '44': G1s, '45': A1, '46':A1s,'47':B1,'48':C2, '49':C2s, '50':D2, '51':D2s, '52': E2, '53': F2, '54': F2s, '55': G2, '56': G2s, '57': A2, '58':A2s,'59':B2,'60':C3, '61':C3s, '62':D3, '63':D3s, '64': E3, '65': F3, '66': F3s, '67': G3, '68': G3s, '69': A3, '70':A3s,'71':B3,'72':C4,'73':C4s,'74':D4,'75':D4s,'76':E4,'77':F4};
-
-
-def play(data):
-    if data[1] == 0:
-        print "STOP " + str(data[0])
-    else:
-        print "START " + str(data[0])
-        dict[str(data[0])].play()
-
-while True:
-    ms_passed = (clk.tick()*1.3)
-
-    for index, tick in enumerate(ticks):
-    	newtick = tick - ms_passed
-    	ticks[index] = newtick
-
-    	#print '.'
-    	if newtick<= 0:
-	        play(events[index].data)
-	        idx = next_noteonevent(idxs[index]+1, tracks[index])
-	        events[index] = tracks[index][idx]
-	        ticks[index] = events[index].tick
-
-	        idxs[index] = idx
+if __name__ == 'main':
+    main()
