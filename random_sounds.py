d50b86767a210dfd9e5ee5d1728220871f8bf65f
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2015-08-12 12:16:15 +0200

    add more random

--- random_sounds.py
+++ random_sounds.py
@@ -1,10 +1,12 @@
 import pygame
 import random
+import glob
 
 class Random:
     def __init__(self):
-        files = ['dada.wav', 'na-hehehe2.wav', 'shh.wav', 'vatten2.wav', 'knack.wav', 'na2.wav', 'skarpt.wav']
-        self.sounds = [pygame.mixer.Sound('Sounds/random/%s' % x) for x in files]
+        files = glob.glob('Sounds/random/*.wav')
+        self.sounds = [pygame.mixer.Sound(x) for x in files]
+
     def play(self, midi_note):
         r = random.randint(1,len(self.sounds))
         self.sounds[r - 1].play()
