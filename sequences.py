c1b0960d254c2cbcf6712c5c940f6345bf9297df
Sven Hellsten <sven.hellsten@gmail.com>
2015-08-12 11:07:26 +0200

    beats now work

--- sequences.py
+++ sequences.py
@@ -45,8 +45,13 @@ G1s = pygame.mixer.Sound("Sounds/g1#.wav")
 A1 = pygame.mixer.Sound("Sounds/A1.wav")
 A1s = pygame.mixer.Sound("Sounds/A1#.wav")
 B1 = pygame.mixer.Sound("Sounds/B1.wav")
+base = pygame.mixer.Sound("Sounds/Trummor/bass-2.wav")
+closed = pygame.mixer.Sound("Sounds/Trummor/closedhat-1.wav")
+kick = pygame.mixer.Sound("Sounds/Trummor/kick-10.wav")
+snare = pygame.mixer.Sound("Sounds/Trummor/snare-8.wav")
 
-tones = {'41': F1, '42': F1s, '43': G1, '44': G1s, '45': A1, '46':A1s,'47':B1,'48':C2, '49':C2s, '50':D2, '51':D2s, '52': E2, '53': F2, '54': F2s, '55': G2, '56': G2s, '57': A2, '58':A2s,'59':B2,'60':C3, '61':C3s, '62':D3, '63':D3s, '64': E3, '65': F3, '66': F3s, '67': G3, '68': G3s, '69': A3, '70':A3s,'71':B3,'72':C4,'73':C4s,'74':D4,'75':D4s,'76':E4,'77':F4};
+
+tones = {'35':base, '40':snare,'41': F1, '42': F1s, '43': G1, '44': G1s, '45': snare, '46':A1s,'47':B1,'48':C2, '49':C2s, '50':D2, '51':D2s, '52': E2, '53': F2, '54': F2s, '55': G2, '56': G2s, '57': A2, '58':A2s,'59':B2,'60':C3, '61':C3s, '62':D3, '63':D3s, '64': E3, '65': F3, '66': F3s, '67': G3, '68': G3s, '69': A3, '70':A3s,'71':B3,'72':C4,'73':C4s,'74':D4,'75':D4s,'76':E4,'77':F4};
 
 def play(event):
     if isinstance(event, midi.NoteOnEvent) and event.data[1] != 0:
@@ -56,7 +61,7 @@ def play(event):
 
 def main():
 
-    song = midi.read_midifile("Paint_It_Black.mid")
+    song = midi.read_midifile("Paint_It_Black2.mid")
     clk = Clock()
 
     our_tracks = [0,1]
@@ -80,5 +85,5 @@ def main():
                 track['event'] = track['track'][track['event_idx']]
                 track['ticks_left'] = track['event'].tick
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
